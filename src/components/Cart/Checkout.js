51e3ffbff4624b6f07a1c5b9144232b14761ac1d
Salome Charkviani <salo.charkviani@gmail.com>
2022-08-18 22:39:19 +0400

    18.08

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,28 +1,99 @@
-import React from "react";
-import classes from "./Checkout.module.css"
+import { useRef, useState } from "react";
+import classes from "./Checkout.module.css";
 
-const Checkout = props =>{
-return(
-    <form>
-        <div className={classes.control}>
-            <label htmlFor="name">Your Name</label>
-            <input type="text" id="name"></input>
-        </div>
-        <div>
-            <label htmlFor="street">Street</label>
-            <input type="text" id="street" />
-        </div>
-        <div>
-            <label htmlFor="postal">Postal Code</label>
-            <input type="text" id="postal"></input>
-        </div>
-        <div>
-            <label htmlFor="city">City</label>
-            <input type="text" id="city"></input>
-        </div>
-        <button>Confirm</button>
+const isEmpty = (value) => value.trim() === "";
+const fiveChar = (value) => value.trim().length === 5;
+const Checkout = (props) => {
+  const [formValidity, setFormValidity] = useState({
+    name: true,
+    street: true,
+    postalCode: true,
+    city: true,
+  });
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredcity = cityInputRef.current.value;
+
+    const nameIsValid = !isEmpty(enteredName);
+    const streetIsValid = !isEmpty(enteredStreet);
+    const codeIsValid = fiveChar(enteredPostalCode);
+    const cityIsValid = !isEmpty(enteredcity);
+
+    setFormValidity({
+      name: nameIsValid,
+      street: streetIsValid,
+      postalCode: codeIsValid,
+      city: cityIsValid,
+    });
+
+    const formIsValid =
+      nameIsValid && streetIsValid && codeIsValid && cityIsValid;
+  
+  if(!formIsValid){
+    return;
+  }
+  props.onConfirm({
+    name: enteredName,
+    street: enteredStreet,
+    postalCode: enteredPostalCode,
+    city: enteredcity
+  })
+};
+
+  const nameControlClasses = `${classes.control} ${
+    formValidity.name ? "" : classes.invalid
+  }`;
+  const streetControlClasses = `${classes.control} ${
+    formValidity.street ? "" : classes.invalid
+  }`;
+
+  const postalCodeControlClasses = `${classes.control} ${
+    formValidity.postalCode ? "" : classes.invalid
+  }`;
+
+  const cityControlClasses = `${classes.control} ${
+    formValidity.city ? "" : classes.invalid
+  }`;
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={nameControlClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInputRef} />
+        {!formValidity.name && <p>Please, enter a valid name!</p>}
+      </div>
+      <div className={streetControlClasses}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInputRef} />
+        {!formValidity.street && <p>Please, enter a valid street!</p>}
+      </div>
+      <div className={postalCodeControlClasses}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalCodeInputRef} />
+        {!formValidity.postalCode && <p>Please, enter a valid postal code!</p>}
+      </div>
+      <div className={cityControlClasses}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef} />
+        {!formValidity.city && <p>Please, enter a valid city!</p>}
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
-)
+  );
 };
 
 export default Checkout;
